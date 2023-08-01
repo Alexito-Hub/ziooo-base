6b74470f119c3d47ffd8f95820d30e4cad176344
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-08-01 00:42:57 -0500

    new

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { load } = require("./loader")
 
 async function start() {
     try {
-        const botWA = await connectWA()
+        const botWA = await connectWA(start)
         load(botWA)
     } catch (error) {
         console.error("Error al iniciar el bot:", error)
