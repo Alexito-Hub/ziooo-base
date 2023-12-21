e64a4b096a24d22a9712713178a0860866a73050
alessandrovillogas@outlook.es <alessandrovillogas@outlook.es>
2023-12-20 21:00:23 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const font = require("../lib/font")
 const { connect } = require("./components/connection")
 const { load } = require("./components/loader")
-
+const loading = font.loading()
 async function start() {
     try {
-        const loading = font.loading()
+        console.clear()
         console.log(loading)
         const bot = await connect(start);
         load(bot);
